84d8fdb99fc00288918dad427338c7e39d0f0dc6
Your name <example@example.com>
2025-12-23 21:27:38 +0300

    add submarine image

--- submarine.py
+++ submarine.py
@@ -1,13 +1,27 @@
+import os
 import tkinter
+from PIL import ImageTk, Image
 
 
 class Submarine:
     canvas: tkinter.Canvas
     image: int
+    __image_path: str = os.path.join(os.getcwd(), 'submarine.png')
 
     def __init__(self, canvas):
         self.canvas = canvas
-        self.image = self.canvas.create_oval(200, 200, 240, 240, fill='yellow')
+        # При помощи PIL считываем информацию о картинке из файла
+        image_file = Image.open(self.__image_path)
+        # Меняем размер картинки на 100x100
+        image_file = image_file.resize((100, 100), resample=Image.Resampling.LANCZOS)
+        # Конвертируем картинку в объект photo image для tkinter
+        self._image_src = ImageTk.PhotoImage(image_file)
+        # Формируем изображение на холсте, используя объект photo image
+        self.image = self.canvas.create_image(
+            200,
+            200,
+            image=self._image_src
+        )
 
     def __del__(self):
         self.canvas.delete(self.image)
