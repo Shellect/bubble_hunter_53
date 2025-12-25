aa1952269fc860346325b67063c05a701c9b7706
Your name <example@example.com>
2025-12-25 20:44:30 +0300

    add bubble regeneration

--- bubble.py
+++ bubble.py
@@ -26,4 +26,16 @@ class Bubble:
 
     def move(self):
         self.canvas.move(self.image, randint(-2, 2), -self.speed)
+        x1, y1, x2, y2 = self.canvas.coords(self.image)
+        # При достижении верхней границы помещаем пузырь под нижний край окна
+        if y2 < 0:
+            window_height = self.canvas.winfo_height()
+            self.canvas.move(self.image, 0, window_height + randint(10, 20))
+
+        # При достижении левой/правой границы отодвигаем пузырь в область видимости
+        window_width = self.canvas.winfo_width()
+        if x1 > window_width:
+            self.canvas.move(self.image, -self.r, 0)
+        elif x2 < 0:
+            self.canvas.move(self.image, self.r, 0)
 
