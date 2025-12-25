d300d78bcfbf844530a45f635fb38f8a8ed20911
Your name <example@example.com>
2025-12-25 20:45:05 +0300

    add submarine rotation

--- submarine.py
+++ submarine.py
@@ -1,6 +1,7 @@
 import os
 import tkinter
 from PIL import ImageTk, Image
+from PIL.Image import Transpose
 
 
 class Submarine:
@@ -9,18 +10,21 @@ class Submarine:
     __image_path: str = os.path.join(os.getcwd(), 'submarine.png')
 
     def __init__(self, canvas):
+
         self.canvas = canvas
         # При помощи PIL считываем информацию о картинке из файла
         image_file = Image.open(self.__image_path)
         # Меняем размер картинки на 100x100
         image_file = image_file.resize((100, 100), resample=Image.Resampling.LANCZOS)
         # Конвертируем картинку в объект photo image для tkinter
-        self._image_src = ImageTk.PhotoImage(image_file)
+        self._image_src_right = ImageTk.PhotoImage(image_file)
+        # Добавляем вторую зеркальную картинку
+        self._image_src_left = ImageTk.PhotoImage(image_file.transpose(Transpose.FLIP_LEFT_RIGHT))
         # Формируем изображение на холсте, используя объект photo image
         self.image = self.canvas.create_image(
             200,
             200,
-            image=self._image_src
+            image=self._image_src_right
         )
 
     def __del__(self):
@@ -33,5 +37,7 @@ class Submarine:
             self.canvas.move(self.image, 0, 5)
         if event.keysym == "Left":
             self.canvas.move(self.image, -5, 0)
+            self.canvas.itemconfig(self.image, image=self._image_src_left)
         if event.keysym == "Right":
             self.canvas.move(self.image, 5, 0)
+            self.canvas.itemconfig(self.image, image=self._image_src_right)
